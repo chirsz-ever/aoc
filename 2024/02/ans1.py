74b938ca011e18664cb59c48c91106170a9612e2
chirsz-ever <chirsz@foxmail.com>
2024-12-27 20:08:01 +0800

    add 2024/02

---
+++ 2024/02/ans1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys
+
+def is_safe(r: list[int]) -> bool:
+    last: None | int = None
+    direction: None | bool = None
+    for i in range(len(r)):
+        if last is not None:
+            if not 1 <= abs(r[i] - last) <= 3:
+                return False
+            if direction is not None:
+                if (r[i] > last) != direction:
+                    return False
+            direction = r[i] > last
+        last = r[i]
+    return True
+
+def main() -> None:
+    inputFile = 'input'
+    if len(sys.argv) >= 2:
+        inputFile = sys.argv[1]
+    
+    reports: list[list[int]] = []
+
+    with open(inputFile) as fin:
+        for l in fin:
+            l = l.strip()
+            if l:
+                reports.append([int(x) for x in l.split()])
+    # for r in reports:
+    #     print(f'{r} -> {is_safe(r)}')
+    result = sum(1 for r in reports if is_safe(r))
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
